3a1fb67e55189d78a4f2faab049d7c1678878f10
Quan Nguyen <QuanNCT@outlook.com>
2023-09-12 14:38:57 +0700

    Resolve linting error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import "./App.css"
-import React from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
 
   return (
     <>
